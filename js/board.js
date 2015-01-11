a5c0f5d6a4fbd57c18e89c24864dd0628c2fa829
Simon Chaffetz <simis1000@gmail.com>
2015-01-10 19:04:14 -0800

    Abstract out card tags building function

--- js/board.js
+++ js/board.js
@@ -5,12 +5,26 @@ function Board($el) {
   this.deck = new Deck();
 }
 
-Board.prototype.layCards = function () {
-  var cardsString = this.deck.cards.map(function (c) {
-    return "<div class='card'>" + c[0] + ' ' + c[1] + "</div>";
-  }).join('');
+Board.prototype.buildCardTags = function () {
+  return this.deck.cards.map(function (c) {
+    var $div = $('<div>'), suit = c[0], num = c[1];
 
-  this.render(cardsString);
+    $div.addClass('card');
+
+    if (suit == "&hearts;" || suit == "&diams;") {
+      $div.addClass('red');
+    }
+
+    $div.append(suit)
+        .append(num);
+    return $div[0];
+  });
+};
+
+Board.prototype.layCards = function () {
+  var cardTags = this.buildCardTags();
+
+  this.render(cardTags);
 };
 
 var board = new Board($('.content'));
