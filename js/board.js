dfe4a2fb88d699816e53cc6b0c8e9f5be10414f4
Simon Chaffetz <simis1000@gmail.com>
2015-01-10 18:40:09 -0800

    Rename Concentration class Board

--- js/board.js
+++ js/board.js
@@ -1,11 +1,11 @@
-function Concentration($el) {
+function Board($el) {
   this.render = function (content) {
     $el.html(content);
   };
   this.deck = new Deck();
 }
 
-Concentration.prototype.layCards = function () {
+Board.prototype.layCards = function () {
   var cardsString = this.deck.cards.map(function (c) {
     return "<div class='card'>" + c[0] + ' ' + c[1] + "</div>";
   }).join('');
@@ -13,7 +13,7 @@ Concentration.prototype.layCards = function () {
   this.render(cardsString);
 };
 
-var concentration = new Concentration($('.content'));
+var board = new Board($('.content'));
 
-concentration.layCards();
+board.layCards();
 
