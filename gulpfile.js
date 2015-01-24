4a6c5c44e7014554ccef627c1bcc00f0b5a73e7b
Simon Chaffetz <simis1000@gmail.com>
2015-01-23 18:31:34 -0800

    Concat stylesheets with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var paths = {
             './js/dev/player.js',
             './js/dev/*.js',
             './js/vendor/*.js'
-            ]
+            ],
+  styles: ['./css/dev/*.css']
 };
 
 gulp.task('scripts', function () {
@@ -19,9 +20,18 @@ gulp.task('scripts', function () {
     .pipe(gulp.dest('./js/prod'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('styles', function () {
+  return gulp.src(paths.styles)
+    .pipe(sourcemaps.init())
+    .pipe(concat('concentration.all.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./css/prod'));
+});
+
+gulp.task('default', ['scripts', 'styles']);
 
 gulp.task('watch', function () {
   gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.scripts, ['styles']);
 });
 
