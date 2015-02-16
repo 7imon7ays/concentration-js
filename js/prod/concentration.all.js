86f0067e30c8acc45be7564a0330a96b5bcc8090
Simon Chaffetz <simis1000@gmail.com>
2015-02-15 21:39:35 -0800

    Fix computer memory bug

--- js/prod/concentration.all.js
+++ js/prod/concentration.all.js
@@ -157,9 +157,7 @@
   ComputerPlayer.prototype.constructor = ComputerPlayer;
 
   ComputerPlayer.prototype.getInput = function () {
-    var chose = Q.defer(), chosenCard;
-
-    chosenCard = this.findMatchInMemory();
+    var chose = Q.defer(), chosenCard = this.findMatchInMemory();
 
     if (!chosenCard) {
       // Pick a random, non-hidden card if no matches found
@@ -187,35 +185,47 @@
   ComputerPlayer.prototype.watchCards = function () {
     this.$cards.on('showing', function (evnt) {
       var card = evnt.target,
-          cardNumber = $(card).data('number');
+          cardNumber = $(card).data('number'),
+          sameNumberCards = this.memory.find(cardNumber);
 
-      // For some reason removed cards still
-      // appear in the matching loop
-      this.memory.put(card, cardNumber);
+      if (!sameNumberCards) {
+        this.memory.put(cardNumber, [card]);
+      } else if (sameNumberCards.value.indexOf(card) === -1) {
+        sameNumberCards.value.push(card);
+      } else {
+        // If the card was already seen,
+        // move it to the top of the cache
+        this.memory.get(cardNumber);
+      }
+
     }.bind(this));
 
     this.$cards.on('matched', function (evnt) {
-      var card = evnt.target;
+      var card = evnt.target,
+          cardNumber = $(card).data('number'),
+          removedCardsArray = this.memory
+                                  .find(cardNumber).value
+                                  .filter(function (c) {
+                                    return c !== card;
+                                  });
 
-      this.memory.remove(card);
+      this.memory.set(cardNumber, removedCardsArray);
     }.bind(this));
   };
 
   ComputerPlayer.prototype.findMatchInMemory = function () {
-    var seenCards = {}, foundCard;
-    this.memory.forEach(function (card, number) {
-      // Dirty fix to removal problem:
-      // Skip cards here that are already removed
-      if ($(card).hasClass('removed')) {
-      } else if (seenCards[number] && seenCards[number] != card) {
-        var hiddenCard = ($(card).hasClass('hidden') ? card : seenCards[number]);
-        foundCard = hiddenCard;
-      } else {
-        seenCards[number] = card;
-      }
-    });
+    var seenNumbers = this.memory.keys(), sameNumberCards, i;
 
-    return foundCard;
+    for (i = 0; i < seenNumbers.length; i++) {
+      sameNumberCards = this.memory.find(seenNumbers[i]).value;
+      if (sameNumberCards.length > 1) {
+        // Ignore the card currently showing on the board
+        return $(sameNumberCards[0]).hasClass('hidden') ?
+          sameNumberCards[0] : sameNumberCards[1];
+      }
+    }
+
+    return null;
   };
 
   ComputerPlayer.THINK_TIME = 1000;
